a7827b4b2c8e0115246c4a03d012cf81f8fb1836
Alex Nistor <alexmateiciuc06@gmail.com>
2025-01-30 10:59:14 +0100

    added code for number validation

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
+// variables
+
 const input = document.getElementById('number');
+const button = document.getElementById('convert-btn');
+const result = document.getElementById('output');
+
+// functions
 
 // Limit the input to 4 digits
-
-input.addEventListener('input', () => {
-    if (input.value.length > 4) {
+const inputDigitLimit = () => {
+    if (input.value > 4) {
         input.value = input.value.slice(0, 4);
     }
-})
+}
+
+// show result container, if it's hidden
+const showContainer = () => {
+    if (result.classList.contains('hidden')) {
+        result.classList.toggle('hidden');
+    }
+}
+
+// checking if the input is between 1 and 3999
+const inputCheck = () => {
+    const numberInput = Number(input.value);
+     if (numberInput === 0 || !Number.isInteger(numberInput)) {
+        showContainer();
+        result.innerHTML = "Please enter a valid number";
+     } else if (numberInput < 0) {
+        showContainer();
+        result.innerHTML = "Please enter a number greater than or equal to 1";
+     } else if (numberInput > 3999) {
+        showContainer();
+        result.innerHTML = "Please enter a number less than or equal to 3999";
+     }
+};
+
+
+
+
+// event listeners
+
+input.addEventListener('input', inputDigitLimit);
+
+button.addEventListener('click', inputCheck);
+
